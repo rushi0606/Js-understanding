56a11807ee86a77fd7f565efcd9c46333946f7ac
rushi0606 <ringale0606@gmail.com>
2025-12-25 17:21:40 +0530

    variables and datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer code
+
+let name = "Rushikesh"
+let age = 22
+let isLoggedIn = false
+
+let sam;
+
+console.table([age, name, isLoggedIn, sam, typeof(name)])
+/* 
+number
+bigint
+string
+boolean
+object
+undefined
+symbol
+null
+*/
---
+++ basic/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "ringale0606@googole.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+/*accountId = 2  const value can not be changed*/
+
+accountEmail = "abcd@google.com"
+accountPassword = "212121"
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
