2c802b1baf2255db0b7cc30d2cdc1d0e44f9b0b1
rushi0606 <ringale0606@gmail.com>
2025-12-27 11:29:51 +0530

    operation conversion

--- basic/datatype_conversion.js
+++ basic/datatype_conversion.js
@@ -21,3 +21,20 @@ console.log(booleanIsLoggedIn);
    true => 1  false => 0  "" => false  "hitesh" => true
    null => 0
    undefined => NaN  */
+
+
+
+// operations..........
+
+let numvalue = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " rushikesh"
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2 + 2); // when string is first the next values are also considered as string
+console.log(1 + 2 + "2"); // when operation is first then it is performed first and then string is attached
+
