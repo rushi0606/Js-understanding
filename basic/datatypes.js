43b23b64761325a877ecaa20033bf1620f80a0b7
rushi0606 <ringale0606@gmail.com>
2025-12-29 11:58:50 +0530

    datatype summary

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -1,4 +1,7 @@
-"use strict"; //treat all JS code as newer code
+"use strict";
+const { object } = require("motion/react-client");
+
+ //treat all JS code as newer code
 
 let name = "Rushikesh"
 let age = 22
@@ -17,3 +20,28 @@ undefined
 symbol
 null
 */
+
+
+
+//primitive datatype: string, number, boolean, null, undefined, symbol, BigInt
+
+
+//Non-primitive datatype(refrence): array, object, function
+//Array
+const heros = ["Ironman", "Batman"]
+    console.log(heros)
+
+//object
+let myobj = {
+    name:"Rushikesh",
+    age:22
+}
+     console.log(myobj);
+     
+
+// function
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+myFunction();
