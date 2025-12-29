94f287bd1f2dada25f53054edabcc23e4f8c7e22
rushi0606 <ringale0606@gmail.com>
2025-12-29 11:30:51 +0530

    datatype comparision

---
+++ basic/datatype_comparision.js
@@ -0,0 +1,19 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1); //here js automatically converts string into number
+
+console.log("2" === 2); //strictly checks the datatypes 
+
+//== and comparison behaves differently >=,<=,<,> 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // comparision >=,<=,<,> converts null to 0.
+
+
+
