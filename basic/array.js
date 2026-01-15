a47c593d314f721f0acbb91f3c78216b5d5a894d
rushi0606 <ringale0606@gmail.com>
2026-01-15 18:09:36 +0530

    Arrays1

---
+++ basic/array.js
@@ -0,0 +1,40 @@
+//Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr[0]);
+
+const myHeros = ["Ironman", "Thor", "Blackpanther"]
+
+// const myArr2 = new Array(1, 2, 3, 4, 5)
+// console.log(myArr2[1]);
+
+//Array methods
+
+myArr.push(6) //add value at start
+myArr.pop()   //remove value from last
+
+myArr.unshift(9)   //add value at the start of the array
+myArr.shift()    //remove value from start
+
+console.log(myArr);
+
+console.log(myArr.includes(9));  //it will give boolean answer
+console.log(myArr.indexOf(9));    //if the value is not present in array it will give -1
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+console.log(newArr);
+console.log(typeof newArr);   //after joining its type will be string
+console.log(typeof myArr);  //its type is object
+ 
+//slice and splice
+
+console.log("A", myArr);
+const myArr1 = myArr.slice(1, 3)  //give seperate array of slice elem and add first index elem and second last index elem and will not change in orignal arr
+console.log(myArr1);
+console.log(myArr);
+
+console.log("B", myArr);
+const myArr2 = myArr.splice(1, 3)  //give seperate array of splice elem and add first index elem and last index elem as well and will remove splice elem from orignal arr
+console.log(myArr2);
+console.log(myArr);
