226167d56ca9060951f1726e760f05c7d1e692f3
rushi0606 <ringale0606@gmail.com>
2026-01-16 13:20:59 +0530

    Arrays2

--- basic/array.js
+++ basic/array.js
@@ -1,6 +1,7 @@
 //Arrays
 
 const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr.reverse());
 console.log(myArr[0]);
 
 const myHeros = ["Ironman", "Thor", "Blackpanther"]
@@ -39,3 +40,20 @@ const myArr2 = myArr.splice(1, 3)  //it will remove splice elem from orignal arr
 //in splice it is from index number and how many values in last
 console.log(myArr2);
 console.log(myArr);
+
+console.log(myArr.concat(myArr2)); //adds two or more arrays in one array
+const all_new_array = [...myArr, ...myArr2]  //adds two or more arrays using spread operator
+console.log(all_new_array);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)  //flat gives a single array 
+console.log(real_another_array);
+
+console.log(Array.isArray("Rushikesh")) //check whether it is array or not 
+console.log(Array.from("Rushikesh")); //converts into array
+console.log(Array.from({name : "Rushikesh"})); //it will give empty array it can only convert keys into arrays or only values into array after defining
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
