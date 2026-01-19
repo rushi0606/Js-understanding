56e378579b14aafd6eb21a43f3d6fcbd5877486b
rushi0606 <ringale0606@gmail.com>
2026-01-19 13:06:34 +0530

    function scope and inner function scope

--- basic3/scopes.js
+++ basic3/scopes.js
@@ -20,3 +20,35 @@ console.log("INNER:", a); //it will give result 10 i.e. inside blocked scope
 console.log(a); //it will give 200 global scope result
 // console.log(b);
 console.log(c);   //it will run and give result 30 which shouldn't run because variable declared in blocked scope should not run outside it
+
+
+
+
+//function two can access function one but one can't access two
+function one(){
+    const username = "Rushi"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website); //variable can not execute outside the function scope
+    two() 
+}
+one() //function one will not execute if we write the command to run function one in the inner function
+
+
+
+if (true){
+    const username = "Rushi"
+    if(username === "Rushi"){
+        const website = " Youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+ 
