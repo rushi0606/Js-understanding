618cf0af6604a86c32f8ce562861e252432d4e0d
rushi0606 <ringale0606@gmail.com>
2026-02-05 18:51:16 +0530

    array revise

--- basic2/array.js
+++ basic2/array.js
@@ -3,15 +3,18 @@
 const myArr = [0, 1, 2, 3, 4, 5]
 console.log(myArr.reverse());
 console.log(myArr[0]);
+console.log(typeof myArr);  //its type is object
 
-const myHeros = ["Ironman", "Thor", "Blackpanther"]
+// const myHeros = ["Ironman", "Thor", "Blackpanther"]
 
-// const myArr2 = new Array(1, 2, 3, 4, 5)
-// console.log(myArr2[1]);
+const myArr3 = new Array(1, 2, 3, 4, 5)
+console.log(myArr3[1]);
 
-//Array methods
 
-myArr.push(6) //add value at start
+
+// //Array methods
+
+myArr.push(6) //add value at last
 myArr.pop()   //remove value from last
 
 myArr.unshift(9)   //add value at the start of the array
@@ -19,16 +22,19 @@ myArr.shift()    //remove value from start
 
 console.log(myArr);
 
-console.log(myArr.includes(9));  //it will give boolean answer
+console.log(myArr.includes(9));  //it will give boolean answer true or false
 console.log(myArr.indexOf(9));    //if the value is not present in array it will give -1
 console.log(myArr.indexOf(3));
+
 
 const newArr = myArr.join()
 console.log(newArr);
 console.log(typeof newArr);   //after joining its type will be string
 console.log(typeof myArr);  //its type is object
  
-//slice and splice
+
+
+// //slice and splice
 
 console.log("A", myArr);
 const myArr1 = myArr.slice(1, 3)  //give seperate array of slice elem and add first index elem and second last index elem and will not change in orignal arr
@@ -37,7 +43,7 @@ console.log(myArr);
 
 console.log("B", myArr);
 const myArr2 = myArr.splice(1, 3)  //it will remove splice elem from orignal arr
-//in splice it is from index number and how many values in last
+// in splice it is from index number and how many values in last
 console.log(myArr2);
 console.log(myArr);
 
@@ -49,7 +55,7 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 const real_another_array = another_array.flat(Infinity)  //flat gives a single array 
 console.log(real_another_array);
 
-console.log(Array.isArray("Rushikesh")) //check whether it is array or not 
+console.log(Array.isArray("Rushikesh")) //check whether it is array or not and gives output in true or false
 console.log(Array.from("Rushikesh")); //converts into array
 console.log(Array.from({name : "Rushikesh"})); //it will give empty array it can only convert keys into arrays or only values into array after defining
 
