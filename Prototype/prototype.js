71699de2840725c09a28e3898a7bbff1fa2dec54
rushi0606 <ringale0606@gmail.com>
2026-03-16 12:56:14 +0530

    prototype

---
+++ Prototype/prototype.js
@@ -0,0 +1,10 @@
+let username = "chaiaurcode      "
+String.prototype.trueLength = function (){
+    console.log(`${this}`);
+    console.log(`True length is : ${this.trim().length}`);
+    
+}
+username.trueLength()
+
+"Rushikesh".trueLength()
+"Tea".trueLength()
